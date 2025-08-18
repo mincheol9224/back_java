070d388be8a4c1babfbd27e7d8658b6786b61a92
mincheol9224 <007apvm@gmail.com>
2025-08-18 13:59:11 +0900

    day01

---
+++ varible/src/varibleTest/VaribleTest.java
@@ -0,0 +1,21 @@
+package varibleTest;
+
+public class VaribleTest {
+	public static void main(String[] args) {
+// 		초기값
+// 		정수(int) 의 초기값 : 0
+// 		실수 (double) 의 초기밧 : 0.0
+//		문자(char) 의 초기값 : ' '
+//		분자열(String) 의 초기값 : null, ""
+//		논리형(boolean) 의 초기값 : false
+		
+		int num1 = 10;
+		double num2 = 10.7;
+		float num3 = 10.7F;
+		String hello = "안녕";
+		char data = '가';
+		long num4 = 10L;
+				
+	}
+
+}
